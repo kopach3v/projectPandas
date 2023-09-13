b8a541cc0c018f17f50e12662d907d07464754f9
Igor Kopachev <kopachev123@gmail.com>
2023-09-13 21:10:25 +0500

    Добавил продажу, дороже цены покупки

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ def buy_sell(reverse_df):
     count_sell = 0
     count_buy = 0
     pnl = 0.0
-    amount_to_spend_usdt = min(balance_usdt, 100.0)
-
+    amount_to_spend_usdt = 100
 
     buy_wait_trades = []
     buy_sold_trades = []
@@ -44,34 +43,34 @@ def buy_sell(reverse_df):
 
                 print(f"Куплено {amount_bought_btc:.4f} BTC по цене {close} USDT")
 
-        elif len(buy_wait_trades) >0:
+        elif len(buy_wait_trades) > 0:
             print(f"{open}, Зеленая свеча")
-            for item in buy_wait_trades:
-                buy_price = item['price']
-                if buy_price < close and balance_btc > 0:
-                    sell_dict = {}
-                    amount_to_spend_btc = balance_btc
-                    amount_bought_usdt = amount_to_spend_btc * close
-                    balance_btc -= amount_to_spend_btc
-                    balance_usdt += amount_bought_usdt
+            # for item in buy_wait_trades:
+            #     buy_price = item['price']
+            cheapest_item = min(buy_wait_trades, key=lambda x: x['price'])
+            if cheapest_item['price'] < close and balance_btc > 0:
+                sell_dict = {}
+                amount_to_spend_btc = balance_btc
+                amount_bought_usdt = amount_to_spend_btc * close
+                balance_btc -= amount_to_spend_btc
+                balance_usdt += amount_bought_usdt
 
-                    sell_dict['side'] = 'SELL'
-                    sell_dict['time'] = date
-                    sell_dict['price'] = close
-                    sell_dict['amount'] = f'{amount_to_spend_btc:.6f}'
-                    sell_dict['qty'] = f'{amount_bought_usdt:.2f}'
-                    trades.append(sell_dict)
-                    buy_sold_trades.append(buy_wait_trades[0])
-                    buy_wait_trades.pop(0)
-                    count_sell += 1
+                sell_dict['side'] = 'SELL'
+                sell_dict['time'] = date
+                sell_dict['price'] = close
+                sell_dict['amount'] = f'{amount_to_spend_btc:.6f}'
+                sell_dict['qty'] = f'{amount_bought_usdt:.2f}'
+                trades.append(sell_dict)
+                buy_sold_trades.append(buy_wait_trades[0])
+                buy_wait_trades.remove(cheapest_item)
+                count_sell += 1
 
-                    print(f"Продано {amount_to_spend_btc:.4f} BTC по цене {close} USDT")
+                print(f"Продано {cheapest_item}, по цене: {close}")
+                # print(f"Продано {amount_to_spend_btc:.4f} BTC по цене {close} USDT")
 
+    # print(f'Usdt: {balance_usdt}, Btc:{balance_btc}, Продаж:{count_sell}, Покупок: {count_buy}, PNL: {pnl}, {trades}')
+    # print(f'В ожидании на продажу: {buy_wait_trades}, Продано: {buy_sold_trades}')
 
-
-
-    print(f'Usdt: {balance_usdt}, Btc:{balance_btc}, Продаж:{count_sell}, Покупок: {count_buy}, PNL: {pnl}, {trades}')
-    print(f'В ожидании на продажу: {buy_wait_trades}, Продано: {buy_sold_trades}')
 
 buy_sell(reverse_df)
 # fig = go.Figure([go.Candlestick(x=reverse_df['Date'],
